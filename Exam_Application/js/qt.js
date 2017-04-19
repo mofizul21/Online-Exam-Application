57f98a903b6c063e6b92ccb18ca9014f91047797
Mofizul Islam <mofizul21@gmail.com>
2017-04-19 07:27:26 +0600

    Online Exam Application With JavaScirpt.
    Live demo: http://themepack.net/aaa/exam-application

---
+++ Exam_Application/js/qt.js
@@ -0,0 +1,7 @@
+var questions = [
+	[ "What is the capital city of BD?", "Khulna", "Dhaka", "Razshahi", "B" ],
+	[ "Who is US President?", "Barak Obama", "Hillary Clinton", "Trump", "C" ],
+	[ "Who is the write of Gitanjali?", "Kazi Nazrul", "Rabindranath Thakur", "Jibonando Dash", "B" ],
+	[ "What is 8 / 4?", "2", "14", "28", "A" ],
+	[ "White is the symbol of...", "Peace", "War", "Happy", "A" ]
+];
